88e3939833908d6aac57ceb013bd65aeeaffe124
Hunter Monroe <huntis@outlook.com>
2022-09-26 14:25:37 -0600

    Refactor traveler and repository

--- src/traveler.js
+++ src/traveler.js
@@ -1,8 +1,8 @@
 class Traveler {
   constructor(data) {
     this.id = data.id, 
-    this.name = data.name;
-    this.travelerType = data.travelerType;
+    this.name = data.name,
+    this.travelerType = data.travelerType
   }
   findTravelerName() {
     return `Hello, ${this.name}`;
--- test/repository-tests.js
+++ test/repository-tests.js
@@ -1,53 +1,59 @@
-import chai from 'chai';
+import chai from "chai";
 const expect = chai.expect;
-import Traveler from '../src/traveler.js'
-import Repository from '../src/Repository.js'
-import destinationData from '../src/data/destination-mock.js'
-import tripsData from '../src/data/trip-mock.js'
-import travelersData from '../src/data/traveler-mock.js'
+import Traveler from "../src/traveler.js";
+import Repository from "../src/Repository.js";
+import destinationData from "../src/data/destination-mock.js";
+import tripsData from "../src/data/trip-mock.js";
+import travelersData from "../src/data/traveler-mock.js";
 // import trips from'../src/data/traveler-mock.js'
 
 // console.log(trips)
-describe('Repository', function() {
+describe("Repository", function () {
   let traveler1;
-  let traveler2
+  let traveler2;
   let travelerRepo;
   let destinationRepo;
-  let tripRepo;
 
   beforeEach(() => {
     traveler1 = new Traveler(travelersData[0]),
     traveler2 = new Traveler(travelersData[1]),
-    travelerRepo = new Repository(travelersData)
+    travelerRepo = new Repository(travelersData),
     destinationRepo = new Repository(destinationData),
     tripRepo = new Repository(tripsData)
-})
-  it('should be a function', () => {
-    expect(Repository).to.be.a('function')
   });
-  it('should hold repo data', () => {
-    
-  })
 
+  it("should be a function", () => {
+    expect(Repository).to.be.a("function");
+  });
+
+  it("should be able to find user data given a user ID", () => {
+    traveler1 = travelerRepo.findUser(1, "id");
+    traveler2 = travelerRepo.findUser(2, "id");
+    expect(traveler1[0]).to.equal(travelersData[0]);
+    expect(traveler2[0]).to.equal(travelersData[1]);
+  });
   
-  it('should be able to find user data given a user ID', () => {
-  traveler1 = travelerRepo.findUser(1 ,'id')
-  traveler2 = travelerRepo.findUser(2, 'id')
-  expect(traveler1[0]).to.equal(travelersData[0])
-  expect(traveler2[0]).to.equal(travelersData[1])
-  });
-  it('should be able to find destinations', () => {
-    traveler1 = travelerRepo.findTravelerDestinations(tripsData)
-   
-  expect(travelerRepo.findTravelerDestinations(tripsData)).to.deep.equal([{  
-    id: 2,
-    name: "Rachael Vaughten",
-    travelerType: "thrill-seeker"
-    }])
+  it("should be able to find destinations", () => {
+    traveler1 = travelerRepo.findTravelerDestinations(tripsData);
+
+    expect(travelerRepo.findTravelerDestinations(tripsData)).to.deep.equal([
+      {
+        id: 2,
+        name: "Rachael Vaughten",
+        travelerType: "thrill-seeker",
+      },
+    ]);
   });
 
-  it('should be able to get all destination and sort alphabetically', () => {
-  let allDestinations =  destinationRepo.findAllDestinations(destinationRepo)
-  expect(allDestinations).to.deep.equal(['Cartagena, Colombia', 'Jakarta, Indonesia', 'Lima, Peru', 'Madrid, Spain', 'Stockholm, Sweden', 'Sydney, Austrailia'])
+  it("should be able to get all destination and sort alphabetically", () => {
+    let allDestinations = destinationRepo.findAllDestinations(destinationRepo);
+    expect(allDestinations).to.deep.equal([
+      "Cartagena, Colombia",
+      "Jakarta, Indonesia",
+      "Lima, Peru",
+      "Madrid, Spain",
+      "Stockholm, Sweden",
+      "Sydney, Austrailia",
+    ]);
   });
-})
+});
